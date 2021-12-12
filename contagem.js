54899cc897754eb78dac5a9a75f7363603f159d4
edemuner <eduardood15@hotmail.com>
2021-12-12 16:28:43 -0300

    código pronto

---
+++ contagem.js
@@ -0,0 +1,13 @@
+let totalDeAlunos = 100
+
+for (i = 0; i <= totalDeAlunos; i++){
+    if (i == 0){
+        console.log("Zero")
+    }
+    else if (i % 2 == 0){
+        console.log("Par")
+    }
+    else if (i % 2 != 0){
+        console.log("Ímpar")
+    }
+}
